cfb55e3f0ee88c657fd4ec0f5a92cc70bfb29ca6
Sam Kaspar <samuelckaspar@gmail.com>
2021-06-28 11:30:03 -0500

    changes

--- 4. strings .py
+++ 4. strings .py
@@ -1,5 +1,11 @@
-g="yo Samuel, what's up?"
-q="Yo Samuel, what's up?"
+g="yo samuel, what's up?"
+q="WWWEYBT&IGHG)(*^%$@#@!?"
 print(g.capitalize())
 print(q.count("a"))
 print(g.endswith("p"))
+print(g.endswith("?"))
+print(q.find("w"))
+print(g.upper())
+print(q.lower())
+print(g.islower())
+print(q.isupper())
