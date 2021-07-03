fdd4655374ef5e42ab5101c895d154adce44a6f0
Sam Kaspar <samuelckaspar@gmail.com>
2021-07-03 15:06:02 -0500

    changes

---
+++ 6. Numbers and Floats .py
@@ -0,0 +1,18 @@
+print(f"Hi, my name is Samuel and I am {11}, close to 12, in four more years, 16!")
+print(3+8)
+print(22/2)
+print(23//2)
+print(19-8)
+print(3*4)
+print(61%25)
+print(3*4)
+print(4**2)
+
+
+print("Hi "*10)
+
+print(type(3.00))
+print(type(3))
+print(type(3.14))
+print(type("3.14"))
+print(type("3"))
