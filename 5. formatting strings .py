adf3b7066d4821b2a8388d63b30901f2c3ab3e91
Sam Kaspar <samuelckaspar@gmail.com>
2021-07-07 11:38:05 -0500

    changes

--- 5. formatting strings .py
+++ 5. formatting strings .py
@@ -1,6 +1,7 @@
-
+# Assigning two variables to strings
 person="Iron Man"
 day="day!!!"
-
+# Using 'Format'
 print(f"{person} saved the {day}")
+print("{0} saved the {1}".format(person,day))
 
--- 6. Numbers and Floats .py
+++ 6. Numbers and Floats .py
@@ -1,18 +1,52 @@
-print(f"Hi, my name is Samuel and I am {11}, close to 12, in four more years, 16!")
+# Using 'Format'(f) to perform math inside the string and printing out "Hi, my name is Samuel and I am 11, close to 12, in four more years, 16!"
+print(f"Hi, my name is Samuel and I am {5+6}, close to {6*2}, in four more years, {4**2}!")
+
+# Printing the answer to the math problem 3+8
 print(3+8)
+# Printing the answer to the math problem 22/2
 print(22/2)
+# Printing the answer to the math problem 22/2 without any remainder
 print(23//2)
+# Printing the answer to the math problem 19-8
 print(19-8)
+# Printing the answer to the math problem 3*4
 print(3*4)
+# Printing the answer to the math problem the remainder of 61/25
 print(61%25)
+# Printing the answer to the math problem 3*4
 print(3*4)
+# Printing the answer to the math problem 4 to the power of 2
 print(4**2)
 
 
+# Printing "Hi " ten times
 print("Hi "*10)
 
+
+# Printing out the type of 3.00
 print(type(3.00))
+# Printing out the type of 3
 print(type(3))
+# Printing out the type of 3.14
 print(type(3.14))
+# Printing out the type of "3.14"
 print(type("3.14"))
+# Printing out the type of "3"
 print(type("3"))
+# Printing out the type of True
+print(type(True))
+# Printing out the type of False
+print(type(False))
+
+
+# Printing out the integer(number) version of True and False
+print(int(True))
+print(int(False))
+
+# Printing out the boolean version of 3.14, 3, 3.00, -3, and 0
+print(bool(3.14))
+print(bool(3))
+print(bool(3.00))
+print(bool(-3))
+print(bool(0))
+
